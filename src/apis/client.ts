d796391a10e05f93bea04aa1cc528b889cdec757
JerryP <abrcdf1023@gmail.com>
2023-12-06 00:23:58 +0800

    Refactor API client function name

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,8 +1,8 @@
-const apiClient =  <D,>(url: string) => fetch(url, {
+const client =  <D,>(url: string) => fetch(url, {
   headers: {
     accept: 'application/json',
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDI1ZmY3OTI3YjM4YjdjMTFmOTRhZTJiMDA2Y2YwYSIsInN1YiI6IjY1NmExOGIwZDQ2NTM3MDEzOTljYTgxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Md_bcsMtbyiqyqEjGe5uLrkdVer1B92JyYflcxKSl4k'
   }
 }).then(res => res.json() as D)
 
-export default apiClient
+export default client
