f11541713f6570077035ac656c2ce41b52cf0539
JerryP <abrcdf1023@gmail.com>
2023-12-09 14:41:27 +0800

    Fix PosterImage component props

--- src/components/PosterImage/PosterImage.tsx
+++ src/components/PosterImage/PosterImage.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 
@@ -7,12 +7,12 @@ import shimmer from "@/utils/shimmer";
 import toBase64 from "@/utils/toBase64";
 import notfoundImage from "./image-not-found.jpg";
 
-export interface PosterImageProps extends Omit<ImageProps, 'src'> {
+export interface PosterImageProps extends Omit<ImageProps, "src"> {
   src?: string | null;
 }
 
 const PosterImage = (props: PosterImageProps) => {
-  const { src, alt, ...other } = props;
+  const { src, alt, width = 220, height = 330, ...other } = props;
   const [error, setError] = React.useState(!src);
 
   const handleError = () => {
@@ -27,8 +27,8 @@ const PosterImage = (props: PosterImageProps) => {
           ? notfoundImage
           : `https://www.themoviedb.org/t/p/w220_and_h330_face${src}`
       }
-      width={220}
-      height={330}
+      width={width}
+      height={height}
       placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(220, 330))}`}
       onError={handleError}
       alt={alt}
