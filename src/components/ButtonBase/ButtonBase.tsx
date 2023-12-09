3db11b5c0d744fc41c491650b2ba56d22bf0cc20
JerryP <abrcdf1023@gmail.com>
2023-12-09 19:52:50 +0800

    Add ButtonBase component

---
+++ src/components/ButtonBase/ButtonBase.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+import className from 'classnames/bind'
+import styles from './ButtonBase.module.css'
+const cx = className.bind(styles)
+
+export interface ButtonBaseProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const ButtonBase = (props: ButtonBaseProps) => {
+  const { className, ...other } = props
+  return (
+    <button className={cx('root', className)} {...other} />
+  )
+}
+
+export default ButtonBase
