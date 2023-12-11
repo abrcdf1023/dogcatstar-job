fe7050dd01cb7719f0c176288a5a9408000edf6f
JerryP <abrcdf1023@gmail.com>
2023-12-11 23:40:07 +0800

    Fix Button module CSS and remove unnecessary div wrapper

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -12,16 +12,18 @@ export interface ButtonProps extends ButtonBaseProps {
 }
 
 export const Button = (props: ButtonProps) => {
-  const { className, fullWidth, ...other } = props;
+  const { className, fullWidth, style, ...other } = props;
 
   return (
     /**
      * TODO: Remove div wrapper when next.js fix dynamic import order.
      * Css module cause wrong import order due to next.js dynamic import.
      * https://github.com/vercel/next.js/issues/16630
+     *
+     * Or consider to use styled-components.
      */
-    <div className={cx("root")}>
-      <ButtonBase className={cx("btn", fullWidth && "fullWidth")} {...other} />
+    <div className={cx("root", fullWidth && "fullWidth")} style={style}>
+      <ButtonBase className={cx("btn")} {...other} />
     </div>
   );
 };
