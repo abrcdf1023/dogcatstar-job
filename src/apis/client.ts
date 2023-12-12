2a94d5518002d8da328b4fb357d44802c951ba76
JerryP <abrcdf1023@gmail.com>
2023-12-13 00:20:03 +0800

    Update TMDB API token in .env file

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -2,7 +2,7 @@ const client = <D>(url: string) =>
   fetch(url, {
     headers: {
       accept: "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
     },
   }).then((res) => res.json() as D);
 
