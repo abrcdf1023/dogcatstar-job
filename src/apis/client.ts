089a65734a7ed89eaa17c2df1c907e225234c61d
JerryP <abrcdf1023@gmail.com>
2023-12-13 00:07:44 +0800

    Update environment variables and API key

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -2,8 +2,7 @@ const client = <D>(url: string) =>
   fetch(url, {
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDI1ZmY3OTI3YjM4YjdjMTFmOTRhZTJiMDA2Y2YwYSIsInN1YiI6IjY1NmExOGIwZDQ2NTM3MDEzOTljYTgxZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Md_bcsMtbyiqyqEjGe5uLrkdVer1B92JyYflcxKSl4k",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
     },
   }).then((res) => res.json() as D);
 
