0d72055054d272af57ef7d5d69ed70c9d001e6d1
JerryP <abrcdf1023@gmail.com>
2023-12-14 01:05:31 +0800

    Refactor numberCompare function and add additional test cases

--- src/utils/__tests__/numberCompare.test.ts
+++ src/utils/__tests__/numberCompare.test.ts
@@ -7,15 +7,30 @@ describe("numberCompare", () => {
     expect(numberCompare(undefined, undefined)).toBe(0);
   });
 
-  it("should return the difference between a and b when isAsc is true", () => {
+  it("Is ASC by default and should return a - b", () => {
     expect(numberCompare(5, 3)).toBe(2);
+    expect(numberCompare(5, 0)).toBe(5);
+    expect(numberCompare(0, 5)).toBe(-5);
     expect(numberCompare(10, 20)).toBe(-10);
     expect(numberCompare(0, 0)).toBe(0);
+    expect(numberCompare(-1, 0)).toBe(-1);
+    expect(numberCompare(-1, -1)).toBe(0);
+    expect(numberCompare(-1.22, -1.22)).toBe(0);
   });
 
-  it("should return the difference between b and a when isAsc is false", () => {
+  it("When second param is false should be DESC and return b - a", () => {
     expect(numberCompare(5, 3, false)).toBe(-2);
+    expect(numberCompare(5, 0, false)).toBe(-5);
+    expect(numberCompare(0, 5, false)).toBe(5);
     expect(numberCompare(10, 20, false)).toBe(10);
     expect(numberCompare(0, 0, false)).toBe(0);
+    expect(numberCompare(-1, -2, false)).toBe(-1);
+  });
+
+  it("should handle extreme cases", () => {
+    expect(numberCompare(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER - 1)).toBe(1);
+    expect(numberCompare(Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER + 1)).toBe(-1);
+    expect(numberCompare(Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER - 1, false)).toBe(-1);
+    expect(numberCompare(Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER + 1, false)).toBe(1);
   });
 });
