5025b0d71c32e2adb3dfbfe7103c6b996368b472
JerryP <abrcdf1023@gmail.com>
2023-12-15 00:48:23 +0800

    Add meta viewport and title to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <title>DOGCATSTAR</title>
+      </head>
       <body className={inter.className}>
         <SWRProvider>
           <Header />
