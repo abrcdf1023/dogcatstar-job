3605bb44c00b38dd7f1b0fd99c6e842d41769261
JerryP <abrcdf1023@gmail.com>
2024-09-29 18:30:40 +0800

    fix: fetchPopularList with revalidate time

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,9 +1,10 @@
-const client = <D>(url: string) =>
+const client = <D>(url: string, nextConfig?: NextFetchRequestConfig) =>
   fetch(url, {
     headers: {
       accept: "application/json",
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
     },
+    next: nextConfig,
   }).then((res) => res.json() as D);
 
 export default client;
--- src/apis/fetchPopularList.ts
+++ src/apis/fetchPopularList.ts
@@ -3,6 +3,7 @@ import "server-only";
 import { ListResponse, Movie } from "@/interfaces/entities";
 import client from "./client";
 
-const fetchPopularList = () => client<ListResponse<Movie>>("https://api.themoviedb.org/3/movie/popular");
+const fetchPopularList = () =>
+  client<ListResponse<Movie>>("https://api.themoviedb.org/3/movie/popular", { revalidate: 86400 });
 
 export default fetchPopularList;
