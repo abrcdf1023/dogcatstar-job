39aa8b4e687034ffd6572e726187f303c9547f6d
JerryP <abrcdf1023@gmail.com>
2024-09-30 12:26:59 +0800

    chore: test for page speed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import fetchPopularList from "@/apis/fetchPopularList";
-
-import MovieInfiniteScrollList from "@/components/movie/MovieInfiniteScrollList";
-import { Suspense } from "react";
+import Grid from "@/components/common/Grid";
+import MovieSimpleCard from "@/components/common/MovieSimpleCard";
 
 export default async function Home() {
   const data = await fetchPopularList();
 
   return (
-    <Suspense>
-      <MovieInfiniteScrollList mode="popular" fallbackData={[data.results]} />
-    </Suspense>
+    <Grid container>
+      {data.results.map((el) => (
+        <Grid key={el.id}>
+          <MovieSimpleCard
+            href={`/movie/${el.id}`}
+            title={el.title}
+            posterPath={el.poster_path}
+            releaseDate={el.release_date}
+          />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
